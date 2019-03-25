600776cc49c317af2c9f66a892af6f695e8ae8d4
shadowfox3735 <normanpan180@gmail.com>
2019-03-25 23:35:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,8 +5,14 @@ VALUES ("Rowdy", 3, "male", "black", "rowdy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
 VALUES ("Tabitha", 1, "female", "black", "lazy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 5, "male", "brown", "taciturn", 0)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (NULL, 6, "female", "grey", "violent", 1)
