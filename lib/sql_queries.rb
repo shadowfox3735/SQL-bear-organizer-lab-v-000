6c403dd1b8a58efb3d723ac58d4ef91517713767
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 00:10:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT color AS cnt FROM bears 
+  "SELECT color, COUNT(color) AS cnt FROM bears 
   GROUP BY color 
   ORDER BY cnt DESC LIMIT 1"
 end
